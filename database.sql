1e9bd93bab69f5c3c2c0e19086573ad22c552ba7
lotus-npv <lotus.npv@gmail.com>
2024-03-03 00:17:34 +0700

    aa

--- database.sql
+++ database.sql
@@ -3,133 +3,310 @@ drop database if exists lotusocean_jp_app;
 create database lotusocean_jp_app default character set utf8 collate utf8_unicode_ci;
 use lotusocean_jp_app;
 
+-- tao bang danh sach cac doi tuong nguoi dung
+-- create table if not exists users_type (
+--     id int primary key auto_increment,
+--     syndication_id INT,
+--     name VARCHAR(50),
+--     description VARCHAR(200),
+--     create_at DATETIME,
+--     create_by INT,
+--     update_at DATETIME,
+--     update_by INT,
+--     delete_at DATETIME,
+--     flag BOOLEAN,
+--     foreign key (syndication_id) references syndication(id)
+-- );
+
+
+create table if not exists alien_registration_card (
+    id int primary key auto_increment,
+    syndication_id INT,
+    intern_id INT,
+    card_number VARCHAR(50),
+    status_of_residence_id INT,
+    license_date DATETIME,
+    expiration_date DATETIME,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id),
+    foreign key (intern_id) references intern(id)
+);
 
 -- tạo bảng thuc tap sinh
 create table if not exists intern
 (
     id int primary key auto_increment,
-    firstName varchar(55),
-    middleName varchar(55), 
-    lastName varchar(55),
-    username varchar(50) not null,  -- thong tin dang nhap he thong
-    password_encode varchar(100) not null,
-    dob datetime,  -- ngay sinh
-    avata varchar(200), -- link avata
-    avata_update_at datetime, -- ngay cap nhat avata
-    dispatching_company_id int, -- cong ty phai cu
-    receiving_factory_id int, -- xi nghep tiep nhan
-    career_id int, -- nganh nghe
-    address varchar(100), -- dia chi noi o ben Nhat
-    passport_code varchar(50), 
-    alien_card_number varchar(50),
-    create_at datetime,
-    create_by int,
-    update_at datetime,
-    update_by int,
-    foreign key (dispatching_company_id) references dispatching_company (id),
-    foreign key (receiving_factory_id) references receiving_factory (id),
-    foreign key (career_id) references career (id)
+    syndication_id int,
+    avata VARCHAR(100),
+    avata_update_at DATETIME,
+    firstName VARCHAR(20),
+    middleName VARCHAR(20),
+    lastName VARCHAR(20),
+    gender VARCHAR(20),
+    dob DATETIME,
+    career_id INT,
+    passport_code VARCHAR(50),
+    passport_license_date DATETIME,
+    passport_expiration_date DATETIME,
+    status_id INT,
+    alert BOOLEAN,
+    phone_domestically VARCHAR(20),
+    phone_abroad VARCHAR(20),
+    receiving_factory_id INT,
+    dispatching_company_id INT,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id),
+    foreign key (dispatching_company_id) references dispatching_company(id),
+    foreign key (receiving_factory_id) references receiving_factory(id),
+    foreign key (career_id) references career(id),
+    foreign key (status_id) references status(id)
+);
+
+-- tao bang nhan vien
+create table if not exists employee (
+    id int primary key auto_increment,
+    syndication_id INT,
+    firstName VARCHAR(20),
+    middleName VARCHAR(20),
+    lastName VARCHAR(20),
+    gender VARCHAR(10),
+    dob DATE,
+    avata VARCHAR(200),
+    user_type ENUM('syndication','receiving_factory','dispatching_company','intern'),
+    office_id INT, -- (id nơi làm việc)
+    address_id INT,
+    positions_id INT,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id),
+    foreign key (address_id) references address(id),
+    foreign key (positions_id) references positions(id)
+);
+
+-- tao bang danh sach cong ty tiep nhan
+create table if not exists receiving_factory (
+    id int primary key auto_increment,
+    syndication_id INT,
+    logo VARCHAR(100),
+    name VARCHAR(50),
+    tax_code VARCHAR(50),
+    date_of_joining_syndication DATETIME,
+    address_id INT,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
 -- tao bang danh sach cong ty phai cu
 create table if not exists dispatching_company (
     id int primary key auto_increment,
-    name varchar(50), -- ten cong ty
-    nation varchar(50), -- quoc gia
-    company_size varchar(50), -- quy mo cong ty
-    contract_signing_date datetime, -- ngay ky hop dong
-    post-entry_support BOOLEAN, -- ho tro sau nhap canh
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
+    syndication_id INT,
+    logo VARCHAR(100),
+    name VARCHAR(50),
+    tax_code VARCHAR(50),
+    date_of_joining_syndication DATETIME,
+    address_id INT,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
-
--- tao bang danh sach cong ty tiep nhan
-create table if not exists receiving_factory (
+create table if not exists address (
     id int primary key auto_increment,
-    name varchar(50),
-    nation varchar(50), -- quoc gia
-    date_of_joining_syndication datetime, -- ngay tham gia nghiep doan
-    representative int, -- nguoi dai dien
-    employee_in_charge_of_the_job int, -- nhân viên phụ trách công việc
-    employee_in_charge_of_lifestyle int, -- nhân viên phụ trách đời sống
-    address varchar(100),
-    phone_number varchar(20),
-    career_id int, -- nganh nghe chuyen
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
-    foreign key (representative) references users (id),
-    foreign key (employee_in_charge_of_the_job) references users (id),
-    foreign key (employee_in_charge_of_lifestyle) references users (id)
+    syndication_id INT,
+    user_type ENUM('syndication','receiving_factory','dispatching_company','intern'),
+    object_id INT,  -- có thể là id của tts, công ty phái cử, xí nghiệp .
+    nation_id INT,
+    province_id INT,
+    district_id INT,
+    commune_id INT,
+    detail VARCHAR(200),
+    phone_number VARCHAR(20),
+    email VARCHAR(50),
+    fax VARCHAR(50),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
 -- tao bang cho danh sach nganh nghe
 create table if not exists career (
     id int primary key auto_increment,
-    name varchar(50) not null, -- ten nganh nghe
-    description varchar(50), -- mo ta nganh nghe
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
-    delete_at datetime
+    syndication_id INT,
+    name VARCHAR(50),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
+create table if not exists ticket_detail (
+    id int primary key auto_increment,
+    syndication_id INT,
+    ticket_id INT,
+    send_date DATETIME,
+    content VARCHAR(1000),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id),
+    foreign key (ticket_id) references ticket(id)
+);
+
+create table if not exists ticket (
+    id int primary key auto_increment,
+    syndication_id INT,
+    send_date DATETIME,
+    title VARCHAR(100),
+    sender_id INT,
+    receiver_id INT,
+    priority ENUM('Low', 'Medium', 'High'),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id),
+    foreign key (sender_id) references users(id),
+    foreign key (receiver_id) references users(id)
+);
+
+create table if not exists status_of_residence (
+    id int primary key auto_increment,
+    syndication_id INT,
+    name VARCHAR(50),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
+);
+
+create table if not exists status (
+    id int primary key auto_increment,
+    syndication_id INT,
+    name VARCHAR(50),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
+);
+
+create table if not exists positions (
+    id int primary key auto_increment,
+    syndication_id INT,
+    name varchar(50),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
+);
 
 -- tao bang phan quyen
-create table if not exists roles (
+create table if not exists permission (
     id int primary key auto_increment,
-    name varchar(50),
-    description varchar(50),
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
+    syndication_id INT,
+    user_type ENUM('syndication','receiving_factory','dispatching_company','intern'),
+    can_read BOOLEAN,
+    can_edit BOOLEAN,
+    can_delete BOOLEAN,
+    can_create BOOLEAN,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
--- tao bang danh sach cac doi tuong nguoi dung
-create table if not exists users_type (
-    id int primary key auto_increment,
-    name varchar(20), -- phan loai TTS, Nghiep doan, Cong ty phai cu, xi nghiep tiep nhan
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int
-);
 
--- tao bang người dùng (chưa ok)
+-- tao bang người dùng 
 create table if not exists users (
     id int primary key auto_increment,
-    username varchar(20),
-    password_encode varchar(100),
-    users_type_id int, -- phan loai Nghiep doan, Cong ty phai cu, xi nghiep tiep nhan
-    company_name int, -- có thể là tên nghiệp đoàn, công ty phái cử, công ty tiêp nhận
-    position varchar(100), -- chức vụ
-    phone_number varchar(20),
-    email varchar(50),
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
-    foreign key (users_type_id) references users_type (id)
+    syndication_id INT,
+    user_type ENUM('syndication','receiving_factory','dispatching_company','intern'),
+    object_id INT,
+    username VARCHAR(50),
+    password_hash VARCHAR(100),
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN,
+    foreign key (syndication_id) references syndication(id)
 );
 
 -- tao bang phan quyen người dùng
-create table if not exists permission  (
+create table if not exists syndication  (
     id int primary key auto_increment,
-    users_type_id int, -- doi tuong nguoi dung
-    can_create BOOLEAN NOT NULL DEFAULT false,
-    can_read BOOLEAN NOT NULL DEFAULT false,
-    can_update BOOLEAN NOT NULL DEFAULT false,
-    can_delete BOOLEAN NOT NULL DEFAULT false,
-    create_at datetime,
-    creat_by int,
-    update_at datetime,
-    update_by int,
+    name VARCHAR(50),
+    phone VARCHAR(20),
+    email VARCHAR(50),
+    active BOOLEAN,
+    description VARCHAR(200),
+    create_at DATETIME,
+    create_by INT,
+    update_at DATETIME,
+    update_by INT,
+    delete_at DATETIME,
+    flag BOOLEAN
 );
 
 
